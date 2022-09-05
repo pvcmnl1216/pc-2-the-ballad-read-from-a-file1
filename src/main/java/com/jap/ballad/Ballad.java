e5862329bb4159ad2474e5bd54727089d350c195
Philip Meshach <philipmeshach.pm@gmail.com>
2022-09-05 23:51:30 +0530

    final commit

--- src/main/java/com/jap/ballad/Ballad.java
+++ src/main/java/com/jap/ballad/Ballad.java
@@ -1,14 +1,26 @@
 package com.jap.ballad;
 
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
 
 public class Ballad {
-    public String readPoemFromAFile(String fileName){
-       // Write the logic to read from a file
-        return "";
+    public static void main(String[] args) {
+        Ballad ballad = new Ballad();
+        ballad.readPoemFromAFile("src/main/resources/ballad.txt");
     }
 
-
+    public String readPoemFromAFile(String fileName) {
+        String result = "";
+        //noinspection //ReassignedVariable
+        // Write the logic to read from a file
+        try (FileReader reader = new FileReader(fileName);
+             BufferedReader bufferedReader = new BufferedReader(reader)) {
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                result = result.concat(line + "\n");
+            }
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
+        }
+        return result;
+    }
 }
